572551442961aaf1dc6f5c6d325f6050b445d940
Luke <132028994+LukeJLatino@users.noreply.github.com>
2025-11-13 21:02:25 -0500

    Remove timerButton click event listener
    Removed event listener for timerButton click.

--- scripts/popupScript.js
+++ scripts/popupScript.js
@@ -1,9 +1,1 @@
 console.log("popup! again")
-
-document.addEventListener("DOMContentLoaded", function() {
-    var timerButton = document.getElementById("timerButton");
-    timerButton.addEventListener("click", function() {
-        console.log("button clicked!!");
-        eventEmitter.emit("timerButtonClicked");
-    });
-});
